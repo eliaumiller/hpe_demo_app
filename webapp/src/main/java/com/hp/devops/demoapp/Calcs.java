e65b026625acedecc05316d4079eb2939012e222
masrawa <abed.masrawa@hpe.com>
2016-02-28 14:16:29 +0200

    failed  some  tests ...

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -7,11 +7,11 @@ package com.hp.devops.demoapp;
 public class Calcs {
 
 	public static int sum(int a, int b) {     //  1 test covers this
-		return a + b -8 ;
+		return a + b;
 	}
 
 	public static int sub(int a, int b) {     //  3 tests cover this
-		return a - b -9;
+		return a - b;
 	}
 
 	public static boolean equal(int a, int b) {     //  2 tests cover this
@@ -23,6 +23,6 @@ public class Calcs {
 	}
 
 	public static boolean alwaysTrue() {
-		return true;
+		return false;
 	}
 }
