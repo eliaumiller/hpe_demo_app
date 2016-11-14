9a1e7746b3db291557909a4c8a0e212ab5884b96
idogadiel <idogadiel@gmail.com>
2016-11-14 13:19:54 +0200

    disable one test fro check

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -72,20 +72,20 @@ public class TestA {
 
     }
 
-    @Test
-    @Ignore
-    public void testUIcaseA() {
-        System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
-        WebElement query;
-        if(isMusicApp){
-            query = driver.findElement(By.id("bandsList"));
-            Assert.assertEquals(query.getTagName(), "div");
-        } else {
-            query = driver.findElement(By.id("jenkins"));
-            Assert.assertEquals(query.getTagName(), "body");
-        }
-        Assert.assertEquals(query.isDisplayed(), true);
-    }
+//     @Test
+//     @Ignore
+//     public void testUIcaseA() {
+//         System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
+//         WebElement query;
+//         if(isMusicApp){
+//             query = driver.findElement(By.id("bandsList"));
+//             Assert.assertEquals(query.getTagName(), "div");
+//         } else {
+//             query = driver.findElement(By.id("jenkins"));
+//             Assert.assertEquals(query.getTagName(), "body");
+//         }
+//         Assert.assertEquals(query.isDisplayed(), true);
+//     }
 
     @Category(SpecialTests.class)
     @Test
