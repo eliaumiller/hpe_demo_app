08b6310624d58fe930ee1b27948f05312a629400
gullery <yuri.guller@hpe.com>
2016-12-04 12:41:00 +0200

    defect #2001

--- webapp/src/main/java/com/hp/devops/demoapp/Utils.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Utils.java
@@ -6,7 +6,7 @@ public class Utils {
 
 	static String[] nodify(String input) {
 		ArrayList<String> nodes = new ArrayList<String>();
-		System.out.println("touching the failed test sdfsf");
+		System.out.println("touching the failed test sdfssdff");
 		if (input != null) {
 			for (String node : input.split("/")) {
 				if (node.compareTo("") != 0 && node.compareTo("api") != 0) {
