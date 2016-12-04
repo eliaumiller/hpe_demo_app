4f1d421619365ab75d4bed0b8dbf6f080bbfa347
vaingato <vaingart@hpe.com>
2016-12-04 11:05:56 +0200

    defect #1003 - defect for BS feature....

--- webapp/src/main/java/com/hp/devops/demoapp/Utils.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Utils.java
@@ -11,7 +11,7 @@ public class Utils {
 			for (String node : input.split("/")) {
 				if (node.compareTo("") != 0 && node.compareTo("api") != 0) {
 					nodes.add(node);
-					System.out.println("Updated to       fail  test [Abed last touch again .KKKKK.]");
+					System.out.println("Updated to        fail  test [Abed last touch again .KKKKK.]");
 				}
 			}
 		}
