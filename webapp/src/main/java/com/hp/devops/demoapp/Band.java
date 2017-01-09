0793ae0b63adb171230d12c6a53aa8fe9df3f9cc
vaingato <vaingart@hpe.com>
2017-01-09 17:02:56 +0200

    Defect #1002 - test defect

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -35,7 +35,7 @@ public class Band {
 	 * for testing blame committer feature
 	 */
 	public void methodThatThrowsNullPointerException() {
-		System.out.print("going to throw null pointer exception..");
+		System.out.print("going to throw  null pointer exception..");
 		throw new NullPointerException();
 	}
 
