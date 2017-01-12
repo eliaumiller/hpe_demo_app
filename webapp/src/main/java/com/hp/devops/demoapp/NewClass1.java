c44262b7fa8a762a320be88c394541c1585baef4
vaingato <vaingart@hpe.com>
2017-01-12 08:13:02 +0200

    Defect #1002 - test defect

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass1.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass1.java
@@ -4,4 +4,7 @@ package com.hp.devops.demoapp;
  * Created by vaingato on 05/12/2016     .
  */
 public class NewClass1 {
+	public void methodThatThrowsException() {
+		throw new NullPointerException("Exception in class: NewClass1");
+	}
 }
---
+++ webapp/src/test/java/com/hp/devops/demoapp/NewClass1Test.java
@@ -0,0 +1,43 @@
+package com.hp.devops.demoapp;
+
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.security.InvalidParameterException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gullery
+ * Date: 25/11/14
+ * Time: 14:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewClass1Test {
+	//private String NON_RELATED;
+	private JSONObject test;
+
+	@Before
+	public void beforeEach() {
+		test = new JSONObject();
+	}
+
+	@Test
+	public void newClass1TestA() {
+		try {
+			NewClass1 myClass = new NewClass1();
+			myClass.methodThatThrowsException(); // null pointer
+			Assert.fail("the  flow MUST have been fallen before");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getClass(), NullPointerException.class);
+		}
+	}
+
+	@Test
+	public void newClass1TestB() {
+		NewClass1 myClass = new NewClass1();
+		myClass.methodThatThrowsException(); // null pointer
+	}
+}
