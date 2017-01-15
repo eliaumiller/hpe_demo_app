20ffda52cc39e113489a3a81ae84abd16ae5032a
Adi Lev <adi.lev@hpe.com>
2017-01-15 11:56:10 +0200

    defect #3333: bla bla bla

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/SpecialTests.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/SpecialTests.java
@@ -5,7 +5,7 @@ package com.hp.devops.demoapp.tests.ui;
  * User: belozovs
  * Date: 12/7/14
  * Time: 5:20 PM
- * To change this template use File | Settings | File Templates.
+ * To change this template use File  | Settings | File Templates.
  */
 public interface SpecialTests {
 }
