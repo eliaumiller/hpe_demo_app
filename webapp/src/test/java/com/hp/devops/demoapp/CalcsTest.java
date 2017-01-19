347d1c1f665bf062c2d0ff5222b0a0faa0e877e3
gullery <yuri.guller@hpe.com>
2017-01-19 14:31:47 +0200

    defect #2001: some quite lengthy message that i'll preserve here just to be able to see how this is looks like in the email

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. lksdjasldj
+ * Created by  gullery on 17/02/2016.
  */
 public class CalcsTest {
 
