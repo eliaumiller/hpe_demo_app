23c1d4d5730c796fe4187f05093bb192ed2e26e2
hpe-demo-user <yuri.guller@hpe.com>
2017-01-30 11:07:54 +0200

    Mihai commit
    commit m1

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. sfdsfgfgfg
+ * Created by  gullery on 17/02/2016. sfdsfgfgfgfsfsfs
  */
 public class CalcsTest {
 
