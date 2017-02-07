f54340af717f2839ed43bc9be57eabdd3f030cbb
hpe-demo-user <yuri.guller@hpe.com>
2017-02-07 15:57:48 +0200

    Diana committed again
    ha ha

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. comm1 Diana
+ * Created by  gullery on 17/02/2016. Diana committed again
  */
 public class CalcsTest {
 
