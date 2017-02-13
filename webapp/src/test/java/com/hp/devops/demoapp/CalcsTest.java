b9403cdbf738cb7e9002734339ee765eece30f42
hpe-demo-user <yuri.guller@hpe.com>
2017-02-13 14:53:34 +0200

    commit two
    2

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. Diana committed again COMMIT_ONE
+ * Created by  gullery on 17/02/2016. Diana committed again COMMIT_two
  */
 public class CalcsTest {
 
