5e5481f294dd1dc741e32f2f65e6f2e57d3adc1f
hpe-demo-user <yuri.guller@hpe.com>
2017-02-13 14:46:30 +0200

    1 ONE

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. Diana committed again COMMIT_2
+ * Created by  gullery on 17/02/2016. Diana committed again COMMIT_ONE
  */
 public class CalcsTest {
 
