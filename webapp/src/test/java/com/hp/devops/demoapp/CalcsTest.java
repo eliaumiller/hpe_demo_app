d2983f1c7b0ce2338ac21e7d0fe6dd85ad5f7fdd
hpe-demo-user <yuri.guller@hpe.com>
2017-02-22 10:27:55 +0200

    c3

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. Diana 2
+ * Created by  gullery on 17/02/2016. Diana 3
  */
 public class CalcsTest {
 
