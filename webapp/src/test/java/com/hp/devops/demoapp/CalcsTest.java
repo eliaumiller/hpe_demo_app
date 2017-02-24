c8c265666a0608c94e5e03a3d324414861bf6960
hpe-demo-user <yuri.guller@hpe.com>
2017-02-24 11:26:17 +0200

    COMM1

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery on 17/02/2016. Diana C6
+ * Created by  gullery on 17/02/2016. Diana COMM1
  */
 public class CalcsTest {
 
