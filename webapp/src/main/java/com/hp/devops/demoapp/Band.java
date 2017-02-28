c371b82f1765f1032a338406debbe102953ad391
vaingato <vaingart@hpe.com>
2017-02-28 17:37:44 +0200

    User Story #2001

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 import java.security.InvalidParameterException;
 
 /**
- * Created with      IntelliJ IDEA.
+ * Created with       IntelliJ IDEA.
  * User: gullery
  * Date: 24/11/14
  * Time: 10:03
