c1d6efcb9ff4d45eeca1b9f46cce11357a15f01f
hpe-demo-user <yuri.guller@hpe.com>
2017-03-02 16:02:19 +0200

    raluca

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**
- * Created by gullery              o  n 17/02/2016 .test raluca gfgfsfssfsfsfjo;kk
+ * Created by gullery              o  n 17/02/2016 .test
  */
 
 public class Calcs {
