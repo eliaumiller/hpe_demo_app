c9e124f823ea4b3c8be29fff373d86c796a8a2cb
vaingato <vaingart@hpe.com>
2017-03-05 09:01:55 +0200

    User Story #2001

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -31,7 +31,7 @@ public class DataManager {
 			DataManager.servletContext = servletContext;
 			DataManager.loadData();
 		} else {
-			throw new InvalidParameterException("servletContext  must not be null");
+			throw new InvalidParameterException("servletContext   must not be null");
 		}
 	}
 
--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -13,7 +13,7 @@ import java.security.InvalidParameterException;
  * User: gullery
  * Date: 25/11/14
  * Time: 14:21
- * To change this template use File | Settings | File Templates.
+ * To change this  template use File | Settings | File Templates.
  */
 public class BandTest {
 	//private String NON_RELATED;
