5e7f456acf21962dff1630caa3e87011d652c256
vaingato <vaingart@hpe.com>
2017-03-06 08:33:03 +0200

    Defect #1033

--- webapp/src/main/java/com/hp/devops/demoapp/inside/folder/longPath/For/hotspot/chart/SomeClass1.java
+++ webapp/src/main/java/com/hp/devops/demoapp/inside/folder/longPath/For/hotspot/chart/SomeClass1.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp.inside.folder.longPath.For.hotspot.chart;
 
 /**
- * Created by vaingato  on 05/03/2017.
+ * Created by vaingato   on 05/03/2017.
  */
 public class SomeClass1 {
 }
