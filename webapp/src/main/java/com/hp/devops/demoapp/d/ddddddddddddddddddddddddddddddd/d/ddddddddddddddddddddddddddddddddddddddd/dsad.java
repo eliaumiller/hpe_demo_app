193758308ceed884ebb5af846fc331804f9b0245
Raluca <raluca-maria.negrea@hpe.com>
2017-03-13 16:42:44 +0200

    raluca commit without defect

--- webapp/src/main/java/com/hp/devops/demoapp/d/ddddddddddddddddddddddddddddddd/d/ddddddddddddddddddddddddddddddddddddddd/dsad.java
+++ webapp/src/main/java/com/hp/devops/demoapp/d/ddddddddddddddddddddddddddddddd/d/ddddddddddddddddddddddddddddddddddddddd/dsad.java
@@ -2,6 +2,7 @@ package com.hp.devops.demoapp.d.ddddddddddddddddddddddddddddddd.d.dddddddddddddd
 
 /**
  * Created by    vaingato on 05/03/2017.
+ * Testing hotspot widget by Ralu
  */
 public class dsad {
 }
