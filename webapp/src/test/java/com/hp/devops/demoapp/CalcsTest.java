8bd2b24f546f128b4ea99c61cdd64f4e9ec5f7f5
hpe-demo-user <yuri.guller@hpe.com>
2017-03-13 14:37:22 +0200

    Update CalcsTest.java

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery        on 17/02/2016. Diana comment 1
+ * Created by  gullery        on 17/02/2016. Diana comment 1 
  */
 public class CalcsTest {
 
@@ -60,4 +60,5 @@ public class CalcsTest {
 		assertTrue(Calcs.alwaysTrue());
 	}
 	//nissim added comment for regrassion day 14/02/2017 
+	//eli too
 }
