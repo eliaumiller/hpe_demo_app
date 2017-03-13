c8eb17eb557650036f0aa3b7c94ffe0efe809c1d
Raluca <raluca-maria.negrea@hpe.com>
2017-03-13 14:12:20 +0200

    defect #1018: Indicator is displayed before message reached recepient

--- webapp/src/main/java/com/hp/devops/demoapp/abcdefghijklmnopqrstuvwxyz1111/nextPackage/TestClass3.java
+++ webapp/src/main/java/com/hp/devops/demoapp/abcdefghijklmnopqrstuvwxyz1111/nextPackage/TestClass3.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp.abcdefghijklmnopqrstuvwxyz1111.nextPackage;
 
 /**
  * Created by vaingato  on 05/03/2017.
- * Testing hotspot widget by Raluca, again
+ * Testing hotspot widget by Ralu
  */
 public class TestClass3 {
 }
