08d7852e6776268af315f0d02ec4e7a3a03b5ed5
vaingato <vaingart@hpe.com>
2017-03-14 17:44:15 +0200

    Defect #1002

--- webapp/src/main/java/com/hp/devops/demoapp/abcdefghijklmnopqrstuvwxyz1111/nextPackage/TestClass3.java
+++ webapp/src/main/java/com/hp/devops/demoapp/abcdefghijklmnopqrstuvwxyz1111/nextPackage/TestClass3.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp.abcdefghijklmnopqrstuvwxyz1111.nextPackage;
 
 /**
- * Created   by vaingato  on 05/03/2017.
+ * Created    by vaingato  on 05/03/2017.
  */
 public class TestClass3 {
 }
