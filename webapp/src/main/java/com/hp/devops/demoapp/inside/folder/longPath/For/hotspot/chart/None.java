e075ea6ba1299a087d29811d84cf86c7bcf0ed69
vaingato <vaingart@hpe.com>
2017-03-15 14:02:57 +0200

    Defect #1002

--- webapp/src/main/java/com/hp/devops/demoapp/inside/folder/longPath/For/hotspot/chart/None.java
+++ webapp/src/main/java/com/hp/devops/demoapp/inside/folder/longPath/For/hotspot/chart/None.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp.inside.folder.longPath.For.hotspot.chart;
 
 /**
- * Created by vaingato on 15/03/2017.
+ * Created by vaingato on  15/03/2017.
  */
 public class None {
 }
