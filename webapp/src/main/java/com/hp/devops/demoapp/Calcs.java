0b4c7d17eacf076ec293253bf10f95d7cc1fe87b
hpe-demo-user <yuri.guller@hpe.com>
2017-03-17 09:14:07 +0200

    comm1

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**n
- * Created by  gulleryngn                   o2/2016 .ralucabg
+ * Created by  gulleryngn                   o2/2016 .ralucabbvbvbg
  */ 
 
 public class Calcs {
