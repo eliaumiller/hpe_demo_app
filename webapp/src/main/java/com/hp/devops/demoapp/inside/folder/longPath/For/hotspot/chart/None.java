e729e6377203d85aebaad14d88964c881c126e90
Maria <maria@hp.com>
2017-03-17 16:55:43 +0200

    defect #1035: Search by name returns Error when illegal characters are used

--- webapp/src/main/java/com/hp/devops/demoapp/inside/folder/longPath/For/hotspot/chart/None.java
+++ webapp/src/main/java/com/hp/devops/demoapp/inside/folder/longPath/For/hotspot/chart/None.java
@@ -2,6 +2,7 @@ package com.hp.devops.demoapp.inside.folder.longPath.For.hotspot.chart;
 
 /**
  * Created by vaingato on  15/03/2017.
+ * Testing hotspot  widget by Ralu
  */
 public class None {
 }
