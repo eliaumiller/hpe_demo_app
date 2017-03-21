095530265cee8c2ca4d36bbc80fcc141b901934c
vaingato <vaingart@hpe.com>
2017-03-21 15:46:07 +0200

    Defect #1002

--- integration-tests/src/test/java/com/hp/devops/demoapp/ConfigurationService.java
+++ integration-tests/src/test/java/com/hp/devops/demoapp/ConfigurationService.java
@@ -3,7 +3,7 @@ package com.hp.devops.demoapp;
 import javax.annotation.PostConstruct;
 
 /**
- * User:         belozo vs
+ * User:          belozo vs
  * Date: 11/25/14
  * Description
  * Testing hotspot widget by Ralu test test
--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**n
  * Created by      gullery                   o2/2016 .ralucasdgdsfg
- */ 
+ */
 
 public class Calcs {
 
