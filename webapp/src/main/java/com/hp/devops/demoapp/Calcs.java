8f2c4d7411797458b8fc1e4bf7777f301232fb66
vaingato <vaingart@hpe.com>
2017-03-21 08:16:04 +0200

    user story #1003

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**n
- * Created by    gullery                   o2/2016 .raluca
+ * Created by     gullery                   o2/2016 .raluca
  */ 
 
 public class Calcs {
