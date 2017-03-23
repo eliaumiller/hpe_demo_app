cb25ec3954c9c077001b15f3aea2e1b7aa200493
Raluca <raluca-maria.negrea@hpe.com>
2017-03-23 15:18:45 +0200

    defect #1436: Search by name returns Error when illegal characters are used

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**
  *  Created by vaingato              on 05/12/2016.
- *  Testing hotspot  widget by Ralu test
+ *  Testing hotspot  widget by Ralu test test
  */
 public class    NewClass2 {
 }
