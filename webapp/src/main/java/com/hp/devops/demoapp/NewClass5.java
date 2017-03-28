ca1a7d298d0d715ef49bdbba80fdb90d5dadf691
raluca <raluca-maria.negrea@hpe.com>
2017-03-28 18:28:27 +0300

    test

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass5.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass5.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**
  *   Created by vaingato        on 05/12/2016.
- *   test raluca test dfgsdfhg
+ *   test raluca test dfgsdfhg dfgsdfg
  */
 public class NewClass5 {
 }
