ba4d7dcf24d18ef9dd36e26688c714957d3637b1
raluca <raluca-maria.negrea@hpe.com>
2017-03-30 12:34:02 +0300

    defect #1820: Facebook plug in fails on Safari

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass5.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass5.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**
  *   Created by vaingato        on 05/12/2016.
- *   test raluca test dfgsdfhg dfgsdfgK
+ *   test raluca test dfgsdfhg dfgsdfgK test
  */
 public class NewClass5 {
 }
