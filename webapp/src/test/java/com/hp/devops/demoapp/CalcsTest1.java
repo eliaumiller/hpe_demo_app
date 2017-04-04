9550e6d542e44a8df7370e96085850a0fffde5e6
hpe-demo-user <yuri.guller@hpe.com>
2017-04-04 17:28:06 +0300

    defect #271355, defect #269356
    defect #271355, defect #269356

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest1.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest1.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. Diana COMMIT 3
+ * Created by  gullery          on 17/02/2016. DianAAA
  */
 public class CalcsTest {
 
