eccc1fa3f256b16cfbc1b0c2b2b79ae43dbe6d5b
hpe-demo-user <yuri.guller@hpe.com>
2017-04-04 17:40:12 +0300

    1111
    1111111

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest1.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest1.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. DianAAA
+ * Created by  gullery          on 17/02/2016. DianAAA 1111111111
  */
 public class CalcsTest {
 
