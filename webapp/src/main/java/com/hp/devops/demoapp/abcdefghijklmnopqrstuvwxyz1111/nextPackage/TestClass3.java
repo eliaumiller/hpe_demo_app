1ea4b9bddd181917e9d63a51ce4d9219eb0c706e
raluca <raluca-maria.negrea@hpe.com>
2017-04-06 14:52:34 +0300

    defect #1377: money transfer history show only the last week info

--- webapp/src/main/java/com/hp/devops/demoapp/abcdefghijklmnopqrstuvwxyz1111/nextPackage/TestClass3.java
+++ webapp/src/main/java/com/hp/devops/demoapp/abcdefghijklmnopqrstuvwxyz1111/nextPackage/TestClass3.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp.abcdefghijklmnopqrstuvwxyz1111.nextPackage;
 
 /**
  * Created       b  y vaingato  on 05/03/2017.
- * test raluca reagd
+ * test raluca reagd jsdfgsj
  */
 public class TestClass3 {
 }
