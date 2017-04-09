956aa15349541a4eebf7feec5bcfc207d2e1b35e
Shitrit <nissim.shitrit@hpe.com>
2017-04-09 11:50:09 +0300

    adding file for test

---
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestD.java
@@ -0,0 +1,16 @@
+package com.hp.devops.demoapp.tests.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by shitritn on 4/9/2017.
+ */
+public class TestD {
+
+	@Test
+	public void testUIcaseAlwaysPass() {
+		System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
+		Assert.assertTrue(true);
+	}
+}
