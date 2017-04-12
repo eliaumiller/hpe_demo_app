7da4b16462353bb0a723487696513043e7a6a9d4
raluca <raluca-maria.negrea@hpe.com>
2017-04-12 10:57:29 +0300

    test

--- webapp/src/main/java/test/NewClass4.java
+++ webapp/src/main/java/test/NewClass4.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato            on 05/12/2016
- * Testing hotspot  widget by Ralu test tet
+ * Testing hotspot  widget by Ralu test tet test
  */
 public class NewClass4 {
 }
