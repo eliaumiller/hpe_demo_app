8c3a0525f36af532173554f2e6c129dbe076d375
raluca <raluca-maria.negrea@hpe.com>
2017-04-13 17:43:35 +0300

    defect #1032

--- webapp/src/main/java/test/NewClass4.java
+++ webapp/src/main/java/test/NewClass4.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato            on 05/12/2016
- * Testing hotspot  widget by Ralu test tet testferfewfr
+ * Testing hotspot  widget by Ralu test tet
  */
 public class NewClass4 {
 }
