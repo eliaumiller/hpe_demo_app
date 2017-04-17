d10fce0849d2ad909ddda8aa6cbb0430e0a70120
vaingato <vaingart@hpe.com>
2017-04-17 16:52:10 +0300

    user story #1003 risky commit

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**
- *  Created by vaingato                          on 05/12/2016.
+ *  Created by vaingato                           on 05/12/2016.
  *  Testing hotspot  widget by Ralu test
  *  test
  */
