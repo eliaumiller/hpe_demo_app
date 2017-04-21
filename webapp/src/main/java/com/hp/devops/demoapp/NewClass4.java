f3903a1a85a023fba5bf3cfd6c0c7b2e75c4e0c3
alice <alice_demo@nga>
2017-04-21 12:25:59 +0300

    defect #1069

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass4.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass4.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**
  * Created by vaingato            on 05/12/2016
- * Testing hotspot  widget by Ralu test testfghfgh
+ * Testing hotspot  widget by Ralu test
  */
 public class NewClass4 {
 }
