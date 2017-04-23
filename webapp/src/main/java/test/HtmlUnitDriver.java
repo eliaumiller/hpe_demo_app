7fbb49ac522c8bde03965d815ebcfdf05af5062c
vaingato <vaingart@hpe.com>
2017-04-23 17:31:10 +0300

    user story #2037, defect #1007

--- webapp/src/main/java/test/HtmlUnitDriver.java
+++ webapp/src/main/java/test/HtmlUnitDriver.java
@@ -1,7 +1,7 @@
 package test;
 
 /**
- * Created by                 vaingato on 01/01/2017.
+ * Created by                  vaingato on 01/01/2017.
  * test raluca rtert fhfsg
  */
 public class HtmlUnitDriver {
