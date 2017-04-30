2409bdc25b5e2d85750264ec54d871e0ce937598
vaingato <vaingart@hpe.com>
2017-04-30 14:24:09 +0300

    defect #1040

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**n
- * Created by                     gullery                   o2/2016
+ * Created by                      gullery                   o2/2016
  * Testing hotspottttt        widget by Ralu test test
  */
 
