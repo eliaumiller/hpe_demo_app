f6d1e0d7327d87f6590a19be47077276ba8af4eb
hpe-demo-user <yuri.guller@hpe.com>
2017-05-10 15:58:13 +0800

    commit3
    坐看庭前花开花落,笑看天边云卷云舒

--- webapp/src/main/java/com/hp/devops/demoapp/HtmlUnitDriver.java
+++ webapp/src/main/java/com/hp/devops/demoapp/HtmlUnitDriver.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**
- * Created by                     vaingato on 01/01/2017.
+ * Created by                     vaingato on 01/01/2017. 爱吃题
  */
 public class HtmlUnitDriver {
 
