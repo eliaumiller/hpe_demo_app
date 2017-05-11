732d67e4d1eb24d37f107624aa6277ea7a133edd
hpe-demo-user <yuri.guller@hpe.com>
2017-05-11 11:18:56 +0800

    test 测试1
    测试

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**n
  * Created by                      gullery                   o2/2016
- * Testing hotspottttt        widget by Ralu test testyy
+ * Testing hotspottttt        widget by Ralu test testy 测试
  */
 
 public class Calcs {
