263ae191075b9773f0a3b0a0c353bb0f5ec3092c
hpe-demo-user <yuri.guller@hpe.com>
2017-05-12 14:19:33 +0300

    quality story #1008

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass4.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass4.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**
  * Created by vaingato     fd       on 05/12/2016
- * Testing hotspot  widget by Ralu hretherfdfd
+ * Testing hotspot  widget by Ralu hlkretherfdfd
  */
 public class NewClass4 {
 }
