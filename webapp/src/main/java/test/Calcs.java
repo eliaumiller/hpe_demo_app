c6730a1ae6ff3511a04e3c82a5987f4c900efc3d
raluca <raluca-maria.negrea@hpe.com>
2017-05-15 16:25:28 +0300

    defect #1195

--- webapp/src/main/java/test/Calcs.java
+++ webapp/src/main/java/test/Calcs.java
@@ -2,7 +2,7 @@ package test;
 
 /**n
  * Created by                  gullery                   o2/2016
- * Testing hotspot     widget by Ralu ghsdfhd ghsfghfg
+ * Testing hotspot     widget by Ralu
  */
 
 public class Calcs {
