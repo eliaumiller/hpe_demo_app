d9c9c2968de4e0cc5d76119bfb5c59a081e17f6a
raluca <raluca-maria.negrea@hpe.com>
2017-05-16 11:46:29 +0300

    defect #1289

--- webapp/src/main/java/test/NewClass1.java
+++ webapp/src/main/java/test/NewClass1.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato on         05/12/2016     .
- * test raluca test test test fhr
+ * test raluca test test test fhr hfk
  */
 public class NewClass1 {
 	public void methodThatThrowsException() {
