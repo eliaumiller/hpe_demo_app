cf0d9953db554ab51a287a5e15e929b6e7e3e005
raluca <raluca-maria.negrea@hpe.com>
2017-05-16 17:02:27 +0300

    defect #1607

--- webapp/src/main/java/test/NewClass6.java
+++ webapp/src/main/java/test/NewClass6.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato                   on 05/12/2016  .
- * Testing hotspot  widget by Ralujhjh
+ * Testing hotspot  widget by Raluj
  */
 public class NewClass6 {
 }
