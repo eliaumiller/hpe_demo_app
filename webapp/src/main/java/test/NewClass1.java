5692ad89d955fa88c23e66397770514313957a96
raluca <raluca-maria.negrea@hpe.com>
2017-05-16 11:49:56 +0300

    defect #1607

--- webapp/src/main/java/test/NewClass1.java
+++ webapp/src/main/java/test/NewClass1.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato on         05/12/2016     .
- * test raluca test test test fhr hfk
+ * test raluca test test test k
  */
 public class NewClass1 {
 	public void methodThatThrowsException() {
