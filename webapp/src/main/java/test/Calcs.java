4b505e2cc16d708bf144ef2edbf38de121c5e458
raluca <raluca-maria.negrea@hpe.com>
2017-05-18 11:42:38 +0300

    user story #1097

--- webapp/src/main/java/test/Calcs.java
+++ webapp/src/main/java/test/Calcs.java
@@ -2,7 +2,7 @@ package test;
 
 /**n
  * Created by                  gullery                   o2/2016
- * Testing hotspot     widget by Ralu hdf
+ * Testing hotspot     widget by Ralu hdf fgjdfg
  */
 
 public class Calcs {
