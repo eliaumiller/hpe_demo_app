8c87b86e240174bcb1fd0386592e129a33cb09e0
tom <tom@nga>
2017-05-24 13:21:47 +0300

    defect #322001

--- webapp/src/main/java/test/NewClass4.java
+++ webapp/src/main/java/test/NewClass4.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato            on 05/12/2016
- * Testing hotspot  widget by Ralu hdfhsd dfgsdf
+ * Testing hotspot  widget by Ralu hdfhsd
  */
 public class NewClass4 {
 }
