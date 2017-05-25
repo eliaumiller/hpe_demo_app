a22853c881abaae94a4adc55c30e3e1445a65c1b
tom <tom@nga>
2017-05-25 15:15:45 +0300

    defect #1067

--- webapp/src/main/java/test/NewClass6.java
+++ webapp/src/main/java/test/NewClass6.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato                   on 05/12/2016  .
- * Testing hotspot  widget byfghsfgh
+ * Testing hotspot  widget byfghsfgh dfgsdf
  */
 public class NewClass6 {
 }
