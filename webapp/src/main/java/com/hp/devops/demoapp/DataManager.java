970661d8c3a9f600f415d41b2fa2e583c2f6e57c
hpe-demo-user <yuri.guller@hpe.com>
2017-05-27 17:06:36 +0800

    user story #1179, defect #1180
    i would like to see the risky commits.

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 /**
  * Created with IntelliJ     IDEA.
- * User: gullery 中国 test
+ * User: gullery 中国 test again打交道
  * Date: 24/11/14fsfsfdffdfdddgfgfgfg
  * Time: 09:58
  * To change this    template use File | Settings | File Templates.hjhjh
