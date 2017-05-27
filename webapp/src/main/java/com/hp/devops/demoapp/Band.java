35daef420dc492d1090d66253003ab751a18c645
hpe-demo-user <yuri.guller@hpe.com>
2017-05-27 10:59:45 +0800

    Defect #1036
    测试

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -8,7 +8,7 @@ import java.security.InvalidParameterException;
  * Created with           IntelliJdf IDEA.
  * User: gullery
  * Date: 24/11/14 
- * Time: 10:03  Zeugnis testing
+ * Time: 10:03  Zeugnis testing again
  * To change this                     template use File |   Settings | File Templates.
  */
 public class Band {
