615cf7d25ab6fbfe2db0089fd7f462b4ddd2c9e9
hpe-demo-user <yuri.guller@hpe.com>
2017-05-27 11:07:12 +0800

    user story #1032
    特拟定

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp;
 
 /**n
  * Created by                      gullery                   o2/2016
- * gTesting hotspokkkttttkktds大的dsgdg        widget by Ralu Familie mich
+ * gTesting hotspokkktttt中文kt      widget by Ralu Familie mich
  */
 
 public class Calcs {
