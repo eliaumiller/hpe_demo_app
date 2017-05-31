98a7f82646d6081df68511cffb2d0ea2473ddf10
hpe-demo-user <yuri.guller@hpe.com>
2017-05-31 15:37:46 +0800

    Defect #2147
    différence de temps

--- webapp/src/main/java/com/hp/devops/demoapp/HtmlUnitDriver.java
+++ webapp/src/main/java/com/hp/devops/demoapp/HtmlUnitDriver.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**
- * Created by                     vaingatogfgf on 01/01/2017. 爱吃题
+ * Created by                     vaingatogfgf on 01/01/2017. 
  */
 public class HtmlUnitDriver {
 
