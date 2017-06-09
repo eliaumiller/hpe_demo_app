fc8997c7d0c9c4d1681ea5c575a3f740f6f5e952
tom <tom@nga>
2017-06-09 11:30:47 +0300

    defect #1937

--- webapp/src/main/java/test/Calcs.java
+++ webapp/src/main/java/test/Calcs.java
@@ -2,7 +2,7 @@ package test;
 
 /**n
  * Created by                  gullery                   o2/2016
- * Testing hotspots widget by Raluerter HotSpot 2
+ * Testing hotspots widget by Raluca
  */
 
 public class Calcs {
