23d1abc9ca7d50535b14e8b34de23226f15dc1d9
maria <maria@nga>
2017-06-09 11:32:38 +0300

    defect #1937

--- webapp/src/main/java/test/NewClass6.java
+++ webapp/src/main/java/test/NewClass6.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato                   on 05/12/2016  .
- * Testing hotspot  widget byfghsfgh dfgsdf
+ * Testing hotspot  widget
  */
 public class NewClass6 {
 }
