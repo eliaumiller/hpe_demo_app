47650ea7c7dce2d69d0a5002852992b42611c88e
maria <maria@nga>
2017-06-13 18:01:29 +0300

    defect #1030

--- webapp/src/main/java/test/NewClass1.java
+++ webapp/src/main/java/test/NewClass1.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato on         05/12/2016     .
- * test raluca hdfgadf
+ * test raluca
  */
 public class NewClass1 {
 	public void methodThatThrowsException() {
