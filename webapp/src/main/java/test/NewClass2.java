a9c0d1849aecfd718a958e8a1cf20424ff0b5c2b
maria <maria@nga>
2017-06-14 15:18:00 +0300

    defect #1037

--- webapp/src/main/java/test/NewClass2.java
+++ webapp/src/main/java/test/NewClass2.java
@@ -3,7 +3,7 @@ package test;
 /**
  *  Created by vaingato                   on 05/12/2016.
  *  Testing hotspot  widget by Ralu test
- *  test test raluca test
+ *  test test raluca
  */
 public class         NewClass2 {
 }
