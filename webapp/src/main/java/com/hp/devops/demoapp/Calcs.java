4a55e1ccbe130908520c50af4e663e5263ca76bb
Adi Isenberg <adi.isenberg@hpe.com>
2017-06-26 16:35:57 +0300

    user story #1014: some us

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -8,6 +8,7 @@ public class Calcs {
 
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b-8;
+
 	}
 
 	public static int sub(int a, int b) {     //  3 tests cover this
