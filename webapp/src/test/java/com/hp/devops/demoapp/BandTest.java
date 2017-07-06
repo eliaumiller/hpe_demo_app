8d74fd7a683f7a26011949647abd40bce1fda81a
hpe-demo-user <yuri.guller@hpe.com>
2017-07-06 15:36:39 +0800

    defect #5233 risky時間 前

--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -11,8 +11,8 @@ import java.security.InvalidParameterException;
 /**
  * Created with IntelliJ IDEA.
  * User: gullery
- * Date: 25/11/14 铁素体jhkkllp;
- * Time: 14:21嘎嘎嘎
+ * Date: 25/11/14 
+ * Time: 14:21
  * To change this        template use File | Settings | File Templates.  DIANA mmm
  * Testing hotspot widget by Ralu test test test
  */
