1caacdc25cf08338b1b0e69ae1b581fa08d6574e
hpe-demo-user <yuri.guller@hpe.com>
2017-07-26 12:38:46 +0530

    defect #1003 modify

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. testtted in pipeline hadhadasdk
+ * Created by  gullery          on 17/02/2016. testtted in pipeline hadhadasdkfrafra
  * test raluca test
  ****adad t
  */
