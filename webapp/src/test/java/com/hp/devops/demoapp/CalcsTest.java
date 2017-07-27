8fa3f47dad575ebc264ba665ea12df8ee1080739
hpe-demo-user <yuri.guller@hpe.com>
2017-07-27 16:30:38 +0800

    defect #1019 features #1008 epics #1005 ffff

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. testtted in pipeline 1784
+ * Created by  gullery          on 17/02/2016. testtted in pipeline 1784111
  * test raluca test
  ****adad t
  */
