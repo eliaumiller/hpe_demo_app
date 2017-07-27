97b85323a1a47dfee55c50150059faced17c4237
hpe-demo-user <yuri.guller@hpe.com>
2017-07-27 16:12:44 +0800

    defect #1784 fffaasa

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. testtted in pipeline sasaeeeee1112223344
+ * Created by  gullery          on 17/02/2016. testtted in pipeline 1784
  * test raluca test
  ****adad t
  */
