69c70fef8f7e2a8eabbf27fd3b33adc94c36e6b7
hpe-demo-user <yuri.guller@hpe.com>
2017-07-27 15:47:21 +0800

    defect #1026 ffff

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. testtted in pipeline sasa
+ * Created by  gullery          on 17/02/2016. testtted in pipeline sasaeeeee
  * test raluca test
  ****adad t
  */
