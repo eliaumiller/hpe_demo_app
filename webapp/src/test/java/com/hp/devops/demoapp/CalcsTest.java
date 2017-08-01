ae674663c7662e0445083d623ef40b6ad84d3236
hpe-demo-user <yuri.guller@hpe.com>
2017-08-01 14:39:11 +0800

    defect #2261 aaa

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery          on 17/02/2016. testtted in pipeline 
+ * Created by  gullery          on 17/02/2016. testtted in pipeline 111
  * test raluca test test test
  ****adad tgfjhgfkg
  */
