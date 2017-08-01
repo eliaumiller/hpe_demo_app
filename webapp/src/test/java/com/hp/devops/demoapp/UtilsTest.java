1d81dfc27d543f83f6cec6563c4a690b5a1afd5f
hpe-demo-user <yuri.guller@hpe.com>
2017-08-01 17:55:15 +0800

    defect #1440 red

--- webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
- * User: gullery12usqqqq
+ * User: gullery12usqqqqred
  * Date: 25/11/14
  * Time: 13:46
  * To change this     template use File | Settings | File Templates.
