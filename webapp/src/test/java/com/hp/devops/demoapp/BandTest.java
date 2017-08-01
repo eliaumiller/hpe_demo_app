2f6bbe244b418752ff9eda20142d25e9dea73618
hpe-demo-user <yuri.guller@hpe.com>
2017-08-01 16:40:23 +0800

    defect #2274 new33

--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -10,7 +10,7 @@ import java.security.InvalidParameterException;
 
 /**
  * Created with IntelliJ IDEA.
- * User: gullery111222333
+ * User: gullery111222333444
  * Date: 25/11/14 
  * Time: 14:21
  * To change this        template use File | Settings | File Templates. 测试
