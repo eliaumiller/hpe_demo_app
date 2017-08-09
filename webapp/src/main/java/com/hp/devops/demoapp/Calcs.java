871cefaa4a514441f0e5a5189894fdcfe3abd3ab
Adi Isenberg <adi.isenberg@hpe.com>
2017-08-09 16:38:12 +0300

    user story #1052: vbcgfg

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -8,11 +8,11 @@ package com.hp.devops.demoapp;
 public class Calcs {
 
 	public static int sum(int a, int b) {     //  1 test covers this
-		return a + b;
+		return a + b -5;
 	}
 
 	public static int sub(int a, int b) {     //  3 tests cover this
-		return a - b;
+		return a - b -9;
 	}
 
 	public static boolean equal(int a, int b) {     //  2 tests cover this
