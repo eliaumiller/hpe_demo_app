5b6e4cf104e3cc45e5a676c249c24be4f149cb14
vaingato <vaingart@hpe.com>
2017-08-22 13:18:58 +0300

    pass experimental tests

--- webapp/src/test/java/com/hp/devops/demoapp/ExperimentsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/ExperimentsTest.java
@@ -12,25 +12,25 @@ import java.security.InvalidParameterException;
  * Time: 14:48
  * To change this template use File  | Settings | File Templates.
  */
-public class ExperimentsTests {
+public class ExperimentsTest {
 
 	@Test
 	public void dataManagerTestA12() {
-		Assert.assertEquals(false, false);
+		Assert.assertEquals(true, false);
 	}
 
 	@Test
 	public void dataManagerTestB12() {
-		Assert.assertEquals(false, false);
+		Assert.assertEquals(true, false);
 	}
 
 	@Test
 	public void failTestForCoverageAnalysisB12() {
-		Assert.assertEquals(false, false);
+		Assert.assertEquals(true, false);
 	}
 
 	@Test
 	public void dataManagerTestC12() {
-		Assert.assertEquals(false, false);
+		Assert.assertEquals(true, false);
 	}
 }
