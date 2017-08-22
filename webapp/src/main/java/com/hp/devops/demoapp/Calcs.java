00da2dc435859c526fe9ee9d9dc3a3b2c0075c0b
Adi Isenberg <adi.isenberg@hpe.com>
2017-08-22 16:46:24 +0300

    user story #1052: vbcgfg

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -1,14 +1,14 @@
 package com.hp.devops.demoapp;
 
 /**
- * Created by gullery  on 18/02/2016. gfgf   fang1440
+ * Created by gullery on 18/02/2016. gfgf   fang1440
  *
  */
 
 public class Calcs {
 
 	public static int sum(int a, int b) {     //  1 test covers this
-		return a + b;
+		return a + b+5;
 	}
 
 	public static int sub(int a, int b) {     //  3 tests cover this
