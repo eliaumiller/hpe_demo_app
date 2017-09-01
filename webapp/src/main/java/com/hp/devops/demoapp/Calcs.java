19db5bdea62695054cf2c3815b76b27bad45f679
user2 <user2@nga>
2017-09-01 11:46:42 +0300

    test false

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -8,11 +8,11 @@ package com.hp.devops.demoapp;
 public class Calcs {
 
 	public static int sum(int a, int b) {     //  1  test covers this
-		return a + b -1;
+		return a + b;
 	}
 
 	public static int sub(int a, int b) {     //  3 tests cover this
-		return a - b -10;
+		return a - b;
 	}
 
 	public static boolean equal(int a, int b) {     //  2 tests cover this
@@ -20,10 +20,10 @@ public class Calcs {
 	}
 
 	public static boolean nonEqual(int a, int b) {  //  2 tests cover this
-		return (a == b);
+		return (a != b);
 	}
 
 	public static boolean alwaysTrue() {
-		return false;
+		return true;
 	}
 }
