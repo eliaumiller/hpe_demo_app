e336a77196841b428781a88ed2478092fe86b4f0
alex <alex_demo@nga>
2017-09-20 14:22:10 +0300

    test

--- webapp/src/main/java/test/NewClass1.java
+++ webapp/src/main/java/test/NewClass1.java
@@ -2,7 +2,7 @@ package test;
 
 /**
  * Created by vaingato on         05/12/2016     .
- * test raluca test test
+ * test raluca test test test
  */
 public class NewClass1 {
 	public void methodThatThrowsException() {
