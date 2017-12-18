473fcce306ad889a395003f4a126d1aa1070e6b4
tomvin6 <tomvin6@gmail.com>
2017-12-18 15:15:13 +0200

    defect #2017 - bla2

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -13,7 +13,7 @@ import java.security.InvalidParameterException;
  */
 public class Band {
 	public int id;
-	public String name = "10";
+	public String name = "11";
 	public String logo = "";
 	public String song = "";
 	public int votes = 0;
