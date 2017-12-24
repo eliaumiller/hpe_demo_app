4633d2085c166a5c3795271d4111e4754fe76186
tomvin6 <tomvin6@gmail.com>
2017-12-24 16:32:05 +0200

    defect #2017 - bla2

--- webapp/src/test/java/com/hp/devops/demoapp/NewClass1Test.java
+++ webapp/src/test/java/com/hp/devops/demoapp/NewClass1Test.java
@@ -7,6 +7,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.security.InvalidParameterException;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA. Test
@@ -17,27 +18,48 @@ import java.security.InvalidParameterException;
  */
 public class NewClass1Test {
 	//private String NON_RELATED;
-	private JSONObject test;
+	private Random rand = new Random();
+	private int randomNumber;
 
-	@Before
-	public void beforeEach() {
-		test = new JSONObject();
+	public NewClass1Test() {
+		randomNumber = rand.nextInt(2) + 1;
 	}
 
 	@Test
-	public void newClass1TestA() {
-		try {
+	public void randomE1Test() {
+		if (randomNumber == 1) {
 			NewClass1 myClass = new NewClass1();
 			myClass.methodThatThrowsException(); // null pointer
-			Assert.fail("the   flow MUST  have been fallen before");
-		} catch (Exception e) {
-			Assert.assertEquals(false, true);
+		} else {
+			Assert.assertEquals(true, true);
 		}
 	}
 
 	@Test
-	public void newClass1TestB() {
-		NewClass1 myClass = new NewClass1();
-		myClass.methodThatThrowsException(); // null pointer
+	public void randomE2Test() {
+		if (randomNumber == 1) {
+			NewClass1 myClass = new NewClass1();
+			myClass.methodThatThrowsException(); // null pointer
+		} else {
+			Assert.assertEquals(true, true);
+		}
+	}
+	@Test
+	public void randomG1Test() {
+		if (randomNumber == 2) {
+			NewClass1 myClass = new NewClass1();
+			myClass.methodThatThrowsException(); // null pointer
+		} else {
+			Assert.assertEquals(true, true);
+		}
+	}
+	@Test
+	public void randomG2Test() {
+		if (randomNumber == 2) {
+			NewClass1 myClass = new NewClass1();
+			myClass.methodThatThrowsException(); // null pointer
+		} else {
+			Assert.assertEquals(true, true);
+		}
 	}
 }
