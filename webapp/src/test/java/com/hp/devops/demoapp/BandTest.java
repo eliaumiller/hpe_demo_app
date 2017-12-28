172e2ea0567712b60c06e803abb13df765f9f3f9
hpe-demo-user <yuri.guller@hpe.com>
2017-12-28 11:07:20 +0800

    defect #2029
    Cindy田间了一些改动

--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -13,7 +13,7 @@ import java.security.InvalidParameterException;
  * User: gullery12
  * Date: 25/11/14 
  * Time: 14:21
- * To change this        template use File | Settings | File Templates. 测试
+ * To change this        template use File | Settings | File Templates. 测试添加一些东西
  * Testing hotspot widget by Ralu test
  */
 public class BandTest {
