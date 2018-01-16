907a567d0aad7e2cf6729e9a45a89f2f35a153f7
tomvin6 <tomvin6@gmail.com>
2018-01-16 16:41:35 +0200

    defect #1002 - t1

---
+++ webapp/src/main/java/test/MyBlameCommitterByPastTestClass.java
@@ -0,0 +1,21 @@
+package test;
+
+/**
+ *  Created by vaingato                   on 05/12/2016.
+ *  Testing hotspot  widget by Ralu
+ *  test test11
+ */
+public class MyBlameCommitterByPastTestClass {
+
+    // if committed a change to calc, the new analyzer will find you as responsible!
+    public static void blameCommitterPastTestA() {     //  1  test covers this
+        Calcs.randomThrowExceptionMethodA();
+    }
+
+    // if committed a change to calc, the new analyzer will find you as responsible!
+    public static void blameCommitterPastTestB() {     //  1  test covers this
+        Calcs.randomThrowExceptionMethodB();
+    }
+
+
+}
