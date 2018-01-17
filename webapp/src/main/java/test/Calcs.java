d58fda3a0dbb411fad080463daebcee1e62f4874
tomvin6 <tomvin6@gmail.com>
2018-01-17 13:28:07 +0200

    defect #1002 - t1

--- webapp/src/main/java/test/Calcs.java
+++ webapp/src/main/java/test/Calcs.java
@@ -3,14 +3,11 @@ package test;
 import java.util.Random;
 
 /**nfdfdfgfgf
- * Created by    2/2016
+ * Created by     2/2016
  * Testing hotspots widget by Raluca
  */
 
 public class Calcs {
-	private static Random rand = new Random();
-	private static int randomNumber = rand.nextInt(2);
-
 	public static int sum(int a, int b) {     //  1  test covers this
 		return a + b -1;
 	}
@@ -32,13 +29,9 @@ public class Calcs {
 	}
 
 	public static void randomThrowExceptionMethodA() {
-		if (randomNumber > 0) {
 			throw new IllegalArgumentException("you are our of luck");
-		}
 	}
 	public static void randomThrowExceptionMethodB() {
-		if (randomNumber == 0) {
 			throw new IllegalArgumentException("you are our of luck");
-		}
 	}
 }
