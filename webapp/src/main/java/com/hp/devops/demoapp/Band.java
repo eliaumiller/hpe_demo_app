e355b22d57610474cd0aa7fee7357d166ecbf653
tomvin6 <tomvin6@gmail.com>
2018-01-25 15:31:01 +0200

    Revert: defect #1002 - t1

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -5,11 +5,11 @@ import org.json.JSONObject;
 import java.security.InvalidParameterException;
 
 /**vvvv
- * Created with IntelliJdf IDEA.
+ * Created with IntelliJdf  IDEA.
  * User: gullery
  * Date: 24/11/14  的dd
  * Time: 10:03 Zeugnis testing again
- * To change this template use File |  Settings | File Templates.
+ * To change this template use File | Settings | File Templates.
  */
 public class Band {
 	public int id;
