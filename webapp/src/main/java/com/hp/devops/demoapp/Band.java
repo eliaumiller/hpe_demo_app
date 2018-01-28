4e5f58841ab95fff8af910d93d9d8db9d25560f7
tomvin6 <tomvin6@gmail.com>
2018-01-28 08:16:57 +0200

    Revert: defect #1002 - t1

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -8,7 +8,7 @@ import java.security.InvalidParameterException;
  * Created with IntelliJdf  IDEA.
  * User: gullery
  * Date: 24/11/14 的dd
- * Time: 10:03  Zeugnis testing again
+ * Time: 10:03 Zeugnis testing again
  * To change this template use File | Settings | File Templates.
  */
 public class Band {
