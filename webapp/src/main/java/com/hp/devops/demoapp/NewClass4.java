fc19471140fa1e71182f3a83f03932a72b1232a4
tomvin6 <tomvin6@gmail.com>
2018-02-11 16:10:38 +0200

    Revert: defect #1002 - t1

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass4.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass4.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**
- * Created by vaingato     fd       on 05/12/2016
+ * Created by vaingato      fd       on 05/12/2016
  * Testing hotspot  widget by Ralu hlkretherfdfd
  */
 public class NewClass4 {
