8a6ffd4e80dc62c1d1957156556d06d01003807b
tomvin6 <tomvin6@gmail.com>
2018-03-06 13:54:58 +0200

    defect #1003

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass7.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass7.java
@@ -1,7 +1,7 @@
 package com.hp.devops.demoapp;
 
 /**
- * Created by     fdfd bbg                                                     vaingato on 04/04/2017.
+ * Created by      fdfd bbg                                                     vaingato on 04/04/2017.
  */
 public class NewClass7 {
 }
