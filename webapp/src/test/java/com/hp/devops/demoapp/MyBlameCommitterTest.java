b2e9ea648a2d1aa2ced42fd082464bcf2c26deb0
tomvin6 <tomvin6@gmail.com>
2018-04-22 09:41:41 +0300

    defect #2018

--- webapp/src/test/java/com/hp/devops/demoapp/MyBlameCommitterTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/MyBlameCommitterTest.java
@@ -12,12 +12,14 @@ import java.util.Random;
 public class MyBlameCommitterTest {
     // a   b c
 	private static Random rand;
-	private static int randomNumber;
+	private static int randomNumber, randomNumber2, randomNumber3;
 
 	@Before
 	public void beforeEach() {
 		rand = new Random();
 		randomNumber = rand.nextInt(3);
+		randomNumber2 = rand.nextInt(3);
+		randomNumber3 = rand.nextInt(3);
 	}
 
 
@@ -45,6 +47,68 @@ public class MyBlameCommitterTest {
 	public void throwsExceptionTestC() {
 		if (randomNumber == 0) {
 		} else if (randomNumber == 1) {
+			Band band = new Band(null);
+			band.throwsExceptionMethod();
+		} else {
+			Assert.assertEquals(true, false);
+		}
+	}
+
+	@Test
+	public void throwsExceptionTestA2() {
+		if (randomNumber2 == 0) {
+			Band band = new Band(null);
+			band.throwsExceptionMethod();
+		} else if (randomNumber2 == 1) {
+			Assert.assertEquals(1, 2);
+		}
+	}
+
+	@Test
+	public void throwsExceptionTestB2() {
+		if (randomNumber2 == 0) {
+			Assert.assertEquals(true, false);
+		} else if (randomNumber2 == 1) {
+		} else {
+			Band band = new Band(null);
+			band.throwsExceptionMethod();
+		}
+	}
+	@Test
+	public void throwsExceptionTestC2() {
+		if (randomNumber2 == 0) {
+		} else if (randomNumber2 == 1) {
+			Band band = new Band(null);
+			band.throwsExceptionMethod();
+		} else {
+			Assert.assertEquals(true, false);
+		}
+	}
+
+	@Test
+	public void throwsExceptionTestA2() {
+		if (randomNumber3 == 0) {
+			Band band = new Band(null);
+			band.throwsExceptionMethod();
+		} else if (randomNumber3 == 1) {
+			Assert.assertEquals(1, 2);
+		}
+	}
+
+	@Test
+	public void throwsExceptionTestB2() {
+		if (randomNumber3 == 0) {
+			Assert.assertEquals(true, false);
+		} else if (randomNumber3 == 1) {
+		} else {
+			Band band = new Band(null);
+			band.throwsExceptionMethod();
+		}
+	}
+	@Test
+	public void throwsExceptionTestC2() {
+		if (randomNumber3 == 0) {
+		} else if (randomNumber3 == 1) {
 			Band band = new Band(null);
 			band.throwsExceptionMethod();
 		} else {
