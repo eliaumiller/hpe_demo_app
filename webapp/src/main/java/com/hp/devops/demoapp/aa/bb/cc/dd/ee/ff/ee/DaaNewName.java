c8c2f2045ff48ead20c1732844b7448c8a4c6db8
sa <sa@nga>
2018-05-02 10:47:46 +0300

    test

--- webapp/src/main/java/com/hp/devops/demoapp/aa/bb/cc/dd/ee/ff/ee/DaaNewName.java
+++ webapp/src/main/java/com/hp/devops/demoapp/aa/bb/cc/dd/ee/ff/ee/DaaNewName.java
@@ -2,7 +2,7 @@ package com.hp.devops.demoapp.aa.bb.cc.dd.ee.ff.ee;
 
 /**
  * Created by  vaingato             on 05/03/2017. HotSpot 1013
- * test raluca 11
+ * test raluca 11 test
  */
 public class DaaNewName {
 }
