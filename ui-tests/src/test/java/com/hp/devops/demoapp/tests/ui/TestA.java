fce0011bed65d6e2ab8167bbf24f9e12fac18bb4
bratfale <alexandru-dan.bratfalean@hpe.com>
2018-05-07 10:44:47 +0300

    defect #1069, quality story #1403, user story #1149

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -20,8 +20,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  * Date: 25/11/14
  * Time: 17:28
  * To change this template use File        | Settings | File Templates.
- * Testing hotspot widget by Ralu test test test test Dan1234
- */
+ * Testing hotspot widget by Ralu test test test test Dan12345
 public class TestA {
 
     static final boolean isMusicApp = false;
