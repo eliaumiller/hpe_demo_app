25bd64894424541ad3efbacd1c618204b1556abc
stancial <alexander.stanciu@hpe.com>
2018-07-11 18:02:12 +0300

    Xander commits to test #3

--- ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA.java
+++ ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA.java
@@ -20,6 +20,7 @@ import org.testng.annotations.Test;
  * Time:   15:50
  * To chan                                       ge this            template use File | Settings | File Templates.
  * Testing hotspot widget by Ralu test test
+ * Xander Tests Octane Pipelines
  */
 public class TestSuiteA {
 
