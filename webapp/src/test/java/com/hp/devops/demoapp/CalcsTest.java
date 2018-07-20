f5f736d93ea810b90c4b69577691b44d55c4b12a
nume <ab@cd>
2018-07-20 16:51:57 +0300

    defect #1004

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Created by  gullery             on 17/02/2016. testtted in pipeline 111
- * this is new *usr2asdfghjklabcd
+ * this is new *usr2asdfghjklabcdm
  */
 public class CalcsTest {
 	////asssggaga
