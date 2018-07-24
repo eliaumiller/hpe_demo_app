1238e2cdbd35f4f00f6f556868a9723db5809103
sa@nga <sa@nga>
2018-07-24 10:37:18 +0300

    defect #1005

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Created by  gullery             on 17/02/2016. testtted in pipeline 111
- * this is new *usr2asdfghjklabcdmgamaaa
+ * this is new *usr2asdfghjklabcdmgama
  */
 public class CalcsTest {
 	////asssggaga
