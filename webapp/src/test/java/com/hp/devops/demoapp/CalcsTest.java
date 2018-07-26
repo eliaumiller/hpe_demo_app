6d1ab953ca3787b0a843b2882eb0ef837c7ddaf9
user1 <user@1>
2018-07-26 11:09:34 +0300

    message

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Created by  gullery             on 17/02/2016. testtted in pipeline 111
- * this is new *usr2asdfghjklabcdmgamamnnmasafk
+ * this is new *usr2asdfghjklabcdmgamamnnmas
  */
 public class CalcsTest {
 	////asssggaga
