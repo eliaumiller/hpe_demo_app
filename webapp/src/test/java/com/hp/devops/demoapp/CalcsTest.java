fbdd6b06876c4bdc1e3d52ddaef4fbd06292d322
vaingart <vaingart@hpe.com>
2018-10-22 14:25:49 +0300

    this change checks calcs class as part of the user story #1233 enables us

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by  gullery             on 17/02/2016. testtted in pipeline 111
+ * Created by  gullery              on 17/02/2016. testtted in pipeline 111
 <<<<<<< HEAD
  * this is new *asdwej
 =======
