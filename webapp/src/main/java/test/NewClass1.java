9ceabc29af409edd1d4f398353e8b1417707dd64
vaingato <vaingart@microfocus.com>
2018-12-12 17:46:05 +0200

    u.s #2015 - my committer is the man of the world

--- webapp/src/main/java/test/NewClass1.java
+++ webapp/src/main/java/test/NewClass1.java
@@ -8,6 +8,10 @@ public class NewClass1 {
 	public void methodThatThrowsException() {
 		System.out.println("bla1  ");
 		System.out.println("bla2   ");
+		System.out.println("bla1  ");
+		System.out.println("bla2   ");
+		System.out.println("bla1  ");
+		System.out.println("bla2   ");
 		throw new NullPointerException("Exception in class: NewClass1");
 	}
 }
