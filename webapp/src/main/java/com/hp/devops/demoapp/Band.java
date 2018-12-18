45d2dc742ad1949ad1816bbe0ab068455d5f6a1e
user2 <user2@nga>
2018-12-18 14:09:55 +0200

    test

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -10,6 +10,7 @@ import java.security.InvalidParameterException;
  * Date: 24/11/14 的dd
  * Time: 10:03 Zeugnis testing again
  * To change this template use File  | Settings | File Templates.
+ * test
  */
 public class Band {
 	public int id;
