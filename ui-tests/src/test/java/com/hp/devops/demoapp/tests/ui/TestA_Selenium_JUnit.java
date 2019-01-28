7af87042c3b12a3ff8e2863878497880f2112298
cirlea <ioana-malina.cirlea@microfocus.com>
2019-01-28 14:35:35 +0200

    message

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA_Selenium_JUnit.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA_Selenium_JUnit.java
@@ -20,7 +20,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  * Date: 25/11/14
  * Time: 17:28
  * To change this template use File | Settings | File Templates.
- * test raluca
+ * test raluca 123
  * testDan1234
  */
 public class TestA_Selenium_JUnit {
