15270399bde686fd9b62db40dc02048e4ad93472
vaingato <vaingart@microfocus.com>
2019-01-29 15:44:58 +0200

    user story #1002 - test

--- webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
+++ webapp/src/main/java/com/hp/devops/demoapp/NewClass2.java
@@ -1,8 +1,8 @@
 package com.hp.devops.demoapp;
 
 /**
- *  Created by vaifdngato              fdfdf                                   on 05/12/2016.
- *  Testing hotspot  widget Ralu test
+ *  Created by vaifdngato                fdfdf                                   on 05/12/2016.
+ *  Testing hotspot   widget Ralu test
  *  test
  */
 public class          NewClass2 {
