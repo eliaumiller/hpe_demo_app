17a8fe780ddbec67cd6fb9935a321012819f70bb
sa@nga <sa@nga>
2019-01-30 10:06:19 +0200

    testing vulnerabilities

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -7,7 +7,7 @@ import java.security.InvalidParameterException;
 public class Musical {
 	public int id;
 	public String name = "tom               exception";
-	public int age;
+	//public int age;
 	//public int type;
 
 	public Musical() {
@@ -16,7 +16,7 @@ public class Musical {
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name + age );
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name);
 		}
 
 }
