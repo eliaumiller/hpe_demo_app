82f9554102d8a3f76f894286299e958f6e5cc450
sa@nga <sa@nga>
2019-01-30 13:16:04 +0200

    User story #1381

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -7,7 +7,8 @@ import java.security.InvalidParameterException;
 public class Musical {
 	public int id;
 	public String name = "tom               exception";
-	//public int age;
+	public int age;
+	public int type;
 
 
 
@@ -19,7 +20,7 @@ public class Musical {
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name + type);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name + type + age + type);
 		}
 
 }
