2ab066c2f24ca85da3273dc91e36e0d236ab9651
sa@nga <sa@nga>
2019-01-30 11:10:37 +0200

    testing vulnerabilities

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -7,8 +7,8 @@ import java.security.InvalidParameterException;
 public class Musical {
 	public int id;
 	public String name = "tom               exception";
-	//public int age;
-	//public int type;
+	public int age;
+	public int type;
 
 	public Musical() {
 		System.out.println("not        important constructor");
@@ -16,7 +16,7 @@ public class Musical {
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name + age + type);
 		}
 
 }
