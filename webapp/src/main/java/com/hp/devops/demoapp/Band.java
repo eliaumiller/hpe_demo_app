f29078d69d7b0ad56c04bf577779d16875bfc23f
vaingato <vaingart@microfocus.com>
2019-02-24 10:25:39 +0200

    user story #3002

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -14,7 +14,7 @@ import java.security.InvalidParameterException;
  */
 public class Band {
 	 public int id;
-	 public String name =  "1 1";
+	 public String name =  "1  1";
 	 public String logo =  "";
 	 public String song =  "";
 	public int votes = 0;
