d8ef20b21715e395b149629d814a4eb47c9ecdd1
sa@nga <sa@nga>
2019-02-26 10:16:32 +0200

    defect #1005 user story #1006

--- webapp/src/main/java/test/Band.java
+++ webapp/src/main/java/test/Band.java
@@ -26,7 +26,7 @@ public class Band {
 			if (json.has("name")) name = json.getString("name");
 			if (json.has("logo")) logo = json.getString("logo");
 			if (json.has("song")) song = json.getString("song");
-			if (json.has("votes")) votes = json.getInt("votes");
+			if (json.has("votes1")) votes = json.getInt("votes");
 		} else {
 			throw new InvalidParameterException("json must have an id property");
 		}
