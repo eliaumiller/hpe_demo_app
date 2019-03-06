6b9a066dde67361109aed354117c6f81ec32c1eb
sa@nga <sa@nga>
2019-03-06 11:50:52 +0200

    user story #1004

--- webapp/src/main/java/test/Band.java
+++ webapp/src/main/java/test/Band.java
@@ -15,7 +15,7 @@ import java.security.InvalidParameterException;
 public class Band {
 	public int id;
 	public String name =  "1";
-	public String logo =  "1";
+	public String logo =  "";
 	public String song =  "1";
 	public int votes = 0;
 
