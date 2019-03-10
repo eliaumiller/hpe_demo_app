81052119bfc70e30e1a765f5991a3c864c0a1095
eemanuelhpe <eemanuel@hpe.com>
2019-03-10 17:12:50 +0200

    defect #785040

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestA.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+
 /**
  * Created with IntelliJ IDEA.
  * User:   gullery
