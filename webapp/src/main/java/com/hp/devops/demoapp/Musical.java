311a856acd752222e42705d3b966ee9ed0bf4408
user4 <user4@microfocus.com>
2019-03-14 14:59:57 +0200

    testVulnerability

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -16,6 +16,8 @@ public class Musical {
 	public int type10;
 	public int type11;
 
+	public int type12;
+
 
 	public Musical() {
 		System.out.println("not        important constructor");
@@ -23,7 +25,7 @@ public class Musical {
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type + type2 + type3 + type4+ type5 + type6 + type7 + type8 + type9 + type10 + type11);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type + type2 + type3 + type4+ type5 + type6 + type7 + type8 + type9 + type10 + type11 + type12);
 		}
 
 }
