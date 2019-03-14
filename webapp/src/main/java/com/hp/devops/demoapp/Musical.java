a834e1e66e95713b1782d9b3a5e084372088956b
user4 <user4@microfocus.com>
2019-03-14 15:19:58 +0200

    vulnerab

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -7,13 +7,24 @@ public class Musical {
 	public int type1;
 
 
+
+
+
+
+
+
+
+
+	public int type2;
+
+
 	public Musical() {
 		System.out.println("not        important constructor");
 	}
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type2);
 		}
 
 }
