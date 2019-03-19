a174c8f240de7657b6e69e038e5a8f219f380fb1
sa@nga <sa@nga>
2019-03-19 15:27:51 +0200

    vul

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -7,6 +7,7 @@ public class Musical {
 	public int type1;
 	public int type3;
 	public int type9;
+	public int type10;
 
 
 
@@ -28,7 +29,7 @@ public class Musical {
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type2 + type5 + type6 + type3 + type9);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type2 + type5 + type6 + type3 + type9 + type10);
 		}
 
 }
