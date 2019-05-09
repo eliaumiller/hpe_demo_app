51e47ac84063daab7cea2a98819e83f4acf6152b
user4 <user4hpe.com>
2019-05-09 10:26:23 +0300

    us #807337

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -10,6 +10,8 @@ public class Musical {
     public int type7;
     public int type8;
 
+    public int type12;
+
 
 	public Musical() {
 		System.out.println("not        important constructor");
@@ -17,7 +19,7 @@ public class Musical {
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type3 + type15 + type7 + type8);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type3 + type15 + type7 + type8 + type12);
 		}
 
 }
