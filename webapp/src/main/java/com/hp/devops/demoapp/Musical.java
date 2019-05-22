25d3ce82b764fffa6660beab812bf78036e81741
user4 <user4hpe.com>
2019-05-22 10:17:38 +0300

    commit message

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -4,25 +4,25 @@ public class Musical {
 	public int id;
 	public String name = "tom               exception";
 	public int age;
-	public int type1;
-    public int type15;
-    public int type3;
-    public int type7;
-    public int type8;
 
-    public int type12;
-    public int type13;
-    public int type14;
     public int type16;
 
 
+
+
+
+
+
+
+    public int name1;
+    public int name2;
 	public Musical() {
 		System.out.println("not        important constructor");
 	}
 
 
 	public void setMusical() throws ExceptionInInitializerError {
-		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type3 + type15 + type7 + type8 + type12 + type13 + type14 + type16);
+		throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age  + type16 + name1 + name2);
 		}
 
 }
