bfa21540669c65236b3a8e4ab93285b5bf242d3b
mqm_aws_qa8@microfocus.com <mqm_aws_qa8@microfocus.com>
2019-06-27 11:17:32 +0300

    user story #837003

--- webapp/src/main/java/test/Utils.java
+++ webapp/src/main/java/test/Utils.java
@@ -27,7 +27,7 @@ public class Utils {
 		System.out.println("lsdkjfl");
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
-			System.out.print("Hello!");
+			System.out.print(i+1);
 		}
 
 		return true;
