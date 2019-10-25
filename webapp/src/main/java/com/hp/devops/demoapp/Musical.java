c1e6debb833faa243a122558022486cb336393eb
ioana-malina.cirlea@hpe.com ioana-malina.cirlea@hpe.com <ioana-malina.cirlea@microfocus.com>
2019-10-25 17:27:15 +0300

    vulnerability

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -29,6 +29,7 @@ public class Musical {
 
 
     public int vulnerab;
+    public int vul2;
 
 
 	public Musical() {
@@ -44,7 +45,7 @@ public class Musical {
 	public void setMusical() throws ExceptionInInitializerError {
 		throw new ExceptionInInitializerError("some customize exception with the name of " + name  + age  + type16 + name1 +
 				name2 + name3 + name4 + name5 + name6 + name8 + nameVul + vulnerab +
-		nameVul1 + nameVul2 + nameVul3 + nameVul4 + vul);
+		nameVul1 + nameVul2 + nameVul3 + nameVul4 + vul + vul2);
 		}
 
 }
