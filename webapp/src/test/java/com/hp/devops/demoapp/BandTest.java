b1a6cb25ad2ac5dd06a347b5b4023c7a1183066e
ifatgordon <42747049+ifatgordon@users.noreply.github.com>
2019-11-04 13:55:13 +0200

    CalcsTest ExperimentsTest TestC DataManage BandTest

--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -15,6 +15,7 @@ import java.security.InvalidParameterException;
  * Time: 14:21
  * To change this         template use File | Settings | File Templates. 测试添加一些东西
  * Testing hotspot widget by Ralu
+ 
  */
 public class BandTest {
 	//private String NON_RELATED;
