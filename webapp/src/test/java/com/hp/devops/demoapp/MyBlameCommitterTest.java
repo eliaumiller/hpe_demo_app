920fdaf37c083dff63c452944f9f48f11554ff00
ifatgordon <42747049+ifatgordon@users.noreply.github.com>
2019-11-21 10:34:09 +0200

    user story #3001 : calcs experiment , defect #3002 : experiment

--- webapp/src/test/java/com/hp/devops/demoapp/MyBlameCommitterTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/MyBlameCommitterTest.java
@@ -10,7 +10,7 @@ import java.security.InvalidParameterException;
 import java.util.Random;
 
 public class MyBlameCommitterTest {
-    // abc
+    // ab c
 	private static Random rand;
 	private static int randomNumber;
 
