39fd1c23494c9b336b187b78f8fc91c9337f42e0
ifatgordon <42747049+ifatgordon@users.noreply.github.com>
2019-11-28 11:25:38 +0200

    User story #2001 : band fix CalcsTest

--- webapp/src/test/java/com/hp/devops/demoapp/ExperimentsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/ExperimentsTest.java
@@ -11,7 +11,7 @@ import java.util.Random;
  * User:   gullery
  * Date: 25/11/15
  * Time: 14:48 test
- * To change this template use File  | Settings | File Templates.  
+ * To change this  template use File  | Settings | File Templates.  
  */
 public class ExperimentsTest {
 	private Random rand = new Random();
