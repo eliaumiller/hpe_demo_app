e5097571badbb27dc95353b253fdc9f4d4d47b85
User1 <user1@microfocus.com>
2019-12-02 13:43:59 +0200

    As a BandTest la lalalal

--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -74,7 +74,7 @@ public class BandTest {
 		Assert.assertEquals(band.name, "");
 		Assert.assertEquals(band.logo, "");
 		Assert.assertEquals(band.song, "Song");
-		Assert.assertEquals(band.votes, 10);
+		Assert.assertEquals(band.votes, 100);
 	}
 
 	@Test
