b9b1f0cf00b763ec4a3215705e0147884e85eaef
ifatgordon <42747049+ifatgordon@users.noreply.github.com>
2019-12-19 16:19:33 +0200

    defect #3001

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertTrue;
  * Created by gullery on 17/02/2016. testtted in pipeline 111
  * test raluca test
  ****adad 做了更改与测试
- * this is new *usr2  
+ * this is new *usr2   
  */
 public class CalcsTest {
 	////asssggaga
