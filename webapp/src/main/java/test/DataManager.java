c7a44811dd0491e681195e5dde8ec91de417baf3
eemanuelhpe <eemanuel@hpe.com>
2020-01-02 14:39:53 +0200

    moving string x

--- webapp/src/main/java/test/DataManager.java
+++ webapp/src/main/java/test/DataManager.java
@@ -22,6 +22,10 @@ import java.util.ArrayList;
  */
 public class DataManager {
 
+	
+	
+	
+	
 	public String x; 
 	static private boolean initialized = false;
 	static private Object lock = new Object();
