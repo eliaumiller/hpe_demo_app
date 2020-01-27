d6be149ddf63d61b6df51062f8d12e841fb0c4ab
sa@nga <sa@nga>
2020-01-27 18:06:50 +0200

    commit message

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -34,6 +34,10 @@ public class Musical {
     public int vulnerability11;
 
 
+
+    public int vulnerability12;
+
+
 	public Musical() {
 		System.out.println("not important constructor");
 		System.out.println("HEllo World!");
@@ -43,6 +47,6 @@ public class Musical {
 	public void setMusical() throws ExceptionInInitializerError {
 		throw new ExceptionInInitializerError("some customize exception with the name of " + name  + age  + name1 +
 				name2 + name3 + name4 + name5 + name6 + name8 + nameVul + vulnerab +
-		nameVul1 + nameVul2 + nameVul3 + nameVul4 + vul +v + v2 + vulnerability + vulnerability1+vulnerability2+vulnerabilitate+vulnerabilitate2 + vvvvul + vulnerability11);
+		nameVul1 + nameVul2 + nameVul3 + nameVul4 + vul +v + v2 + vulnerability + vulnerability1+vulnerability2+vulnerabilitate+vulnerabilitate2 + vvvvul + vulnerability11 + vulnerability12);
 		}
 }
