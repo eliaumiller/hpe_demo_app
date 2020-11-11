25b7e76d13d26c1370101f35765ab619df84bc10
Nissim Shitrit <nissim.shitrit2@microfocus.com>
2020-11-11 11:59:01 +0200

    add parameterize tests

---
+++ webapp/src/test/java/com/hp/devops/demoapp/JunitTestsWithParameters.java
@@ -0,0 +1,40 @@
+package com.hp.devops.demoapp;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class JunitTestsWithParameters {
+    // @Parameters annotation marks this method as parameters provider
+    @Parameters(name = "Run #Square of : {0}^2={1}")
+    public static Iterable<Object []> data()
+    {
+        return Arrays.asList(new Object[][] { { 1, 1 },
+                { 2, 4 },
+                { 3, 19 },
+                { 4, 16 },
+                { 5, 25 } });
+    }
+
+    // Our two parameters
+    private final int input;
+    private final int resultExpected;
+
+    // Constructor is initialized with one set of parameters every time
+    public JunitTestsWithParameters(final int input, final int result)
+    {
+        this.input = input;
+        this.resultExpected = result;
+    }
+
+    @Test
+    public void testUserMapping() {
+        // You can use here assert also
+        Assert.assertEquals(resultExpected, input * input);
+    }
+}
