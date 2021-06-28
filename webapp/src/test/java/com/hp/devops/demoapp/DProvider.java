2a7bb0bf7e7a1d645d218756084bc62b950f09a1
NShitrit <nissim.shitrit2@microfocus.com>
2021-06-28 12:48:28 +0300

    add class for testing

--- webapp/src/test/java/com/hp/devops/demoapp/DProvider.java
+++ webapp/src/test/java/com/hp/devops/demoapp/DProvider.java
@@ -4,7 +4,7 @@ import org.testng.annotations.DataProvider;
 
 public class DProvider {
     @DataProvider(name = "data provider")
-    public Object[][] dpMethod() {
+    public static Object[][] dpMethod() {
         return new Object[][]{ { 1, 1 },{ 2, 4 },{ 3, 19 },{ 4, 16 },{ 5, 25 } };
 
     }
