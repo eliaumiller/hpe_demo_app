3df13be2ef4f1f6777105132c6348dc834ede1b6
NShitrit <nissim.shitrit2@microfocus.com>
2021-06-30 13:11:31 +0300

    add class for testing

---
+++ ui-tests-testng/src/main/java/com/mf/testng/MathUtils.java
@@ -0,0 +1,7 @@
+package com.mf.testng;
+
+public class MathUtils {
+    public static int square(final int number) {
+        return number * number;
+    }
+}
---
+++ ui-tests-testng/src/test/java/com/mf/testng/test/rest/DProvider.java
@@ -0,0 +1,11 @@
+package com.mf.testng.test.rest;
+
+import org.testng.annotations.DataProvider;
+
+public class DProvider {
+    @DataProvider(name = "data provider")
+    public static Object[][] dpMethod() {
+        return new Object[][]{ { 1, 1 },{ 2, 4 },{ 3, 19 },{ 4, 16 },{ 5, 25 } };
+
+    }
+}
