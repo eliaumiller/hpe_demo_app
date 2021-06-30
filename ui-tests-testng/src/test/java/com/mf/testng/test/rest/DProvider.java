98e568c65fad8873bba0b1e4b2fc9741f44827a3
NShitrit <nissim.shitrit2@microfocus.com>
2021-06-30 16:42:57 +0300

    add class for testing

--- ui-tests-testng/src/test/java/com/mf/testng/test/rest/DProvider.java
+++ ui-tests-testng/src/test/java/com/mf/testng/test/rest/DProvider.java
@@ -1,9 +1,11 @@
 package com.mf.testng.test.rest;
 
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 
 public class DProvider {
     @DataProvider(name = "data provider")
+    @Parameters
     public static Object[][] dpMethod() {
         return new Object[][]{ { 1, 1 },{ 2, 4 },{ 3, 19 },{ 4, 16 },{ 5, 25 } };
 
