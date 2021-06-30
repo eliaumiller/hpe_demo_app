f4c52f811ae8633065fc47e5dea5317171614459
NShitrit <nissim.shitrit2@microfocus.com>
2021-06-30 16:36:51 +0300

    add class for testing

--- webapp/src/test/java/com/hp/devops/demoapp/MathUtilsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/MathUtilsTest.java
@@ -12,7 +12,7 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class MathUtilsTest {
     // @Parameters annotation marks this method as parameters provider
-    @Parameters(name = "")
+    @Parameters
     public static Iterable<Object []> data()
     {
         return Arrays.asList(new Object[][] { { 1, 1 },
